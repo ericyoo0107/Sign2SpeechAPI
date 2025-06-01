0fbce88296ea864bfa01270b7ec5b5e333a56b15
JunHyuk Yu <ericyoo0107@gmail.com>
2025-06-01 17:10:21 +0900

    initial commit

---
+++ src/main/java/api/sign2speechapi/Sign2SpeechApiApplication.java
@@ -0,0 +1,13 @@
+package api.sign2speechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sign2SpeechApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sign2SpeechApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/api/sign2speechapi/Sign2SpeechApiApplicationTests.java
@@ -0,0 +1,13 @@
+package api.sign2speechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sign2SpeechApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
